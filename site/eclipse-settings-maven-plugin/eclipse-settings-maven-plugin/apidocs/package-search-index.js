32d03034185f282711a83283c9a256fb025ba228
Gaël LHEZ <glhez@users.noreply.github.com>
2021-12-16 02:03:38 +0100

    eclipse-settings-maven-plugin v3.5.0

--- site/eclipse-settings-maven-plugin/eclipse-settings-maven-plugin/apidocs/package-search-index.js
+++ site/eclipse-settings-maven-plugin/eclipse-settings-maven-plugin/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.eclipse.scout.mojo.eclipse.settings"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.eclipse.scout.mojo.eclipse.settings"}];updateSearchResults();
