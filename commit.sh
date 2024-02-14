d24532e6ecc80083fcf51fdd0b41491e511656e7
Gaël LHEZ <glhez@users.noreply.github.com>
2024-02-15 00:04:00 +0100

    fix commit.sh

--- commit.sh
+++ commit.sh
@@ -4,4 +4,5 @@ if [[ -z "$ver" ]]; then
   echo "error: missing version."
   exit 2
 fi
+git add releases/com/github/glhez/parent site/com.github.glhez.parent
 git commit -m "com.github.glhez.parent v$ver + site" releases/com/github/glhez/parent site/com.github.glhez.parent
